3da112861e7690a60850dca4855d93d597f14f12
Julia Sperandio Pereira1 <juliasperandio@ibm.com>
2021-12-23 14:03:22 -0300

    feat: lecture should be a value object

--- test/entities/course.spec.ts
+++ test/entities/course.spec.ts
@@ -4,13 +4,23 @@ describe('Course', () => {
     it('should be able to add lectures to modules', () => {
         const module = new Module('Fundamentals')
 
-        const lecture: Lecture = {
-            description: 'Branching',
-            videoUrl: 'https://youtube.com/1234'
-        }
+        const lecture: Lecture = new Lecture('Branching', 'https://youtube.com/1234')
 
         module.add(lecture)
         expect(module.includes(lecture)).toBeTruthy();
+    })
+
+    it('should not be able to add the same lecture twice in a module', () => {
+        const module = new Module('Fundamentals')
+
+        const lecture: Lecture = new Lecture('Branching', 'https://youtube.com/1234')
+
+        const sameLecture: Lecture = new Lecture('Branching', 'https://youtube.com/1234')
+
+        module.add(lecture)
+        module.add(sameLecture)
+        expect(module.includes(lecture)).toBeTruthy();
+        expect(module.numberOfLectures).toBe(1);
     })
 
     it('should be able to add modules to courses', () => {
@@ -18,10 +28,7 @@ describe('Course', () => {
             'Continuous Delivery and DevOps with Azure DevOps: Source Control with Git')
         const module = new Module('Fundamentals')
 
-        const lecture: Lecture = {
-            description: 'Branching',
-            videoUrl: 'https://youtube.com/1234'
-        }
+        const lecture: Lecture = new Lecture('Branching', 'https://youtube.com/1234')
 
         module.add(lecture)
         course.add(module)
